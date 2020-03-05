ea59f9c28a97286f11eb620ae281c7be80ca434d
Polytimos <precieux.nsiemo@ynov.com>
2020-03-05 03:29:02 +0100

    Home page whithout navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <form>
-        <label>
-            Nom :
-            <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Envoyer" />
-        </form>
+
+    <div>
+        <div class="navdbar">
+            <div class="navbar_base" >
+
+            </div>
+        </div>
+        <div className="App">
+            <p>
+                <h2> DEJA UN COMPTE SEPHORA ? </h2>
+            </p>
+            <p>
+                Connectez-vous pour retrouver vos avantages. 
+            </p>
+            <form >
+                <label>
+                    <input type="email" name="name" placeholder="Email*" class="champs"/>
+                </label>
+                <p/>
+                <label>
+                    <input type="password" name="password" placeholder ="Mot de passe*" class="champs" /> 
+                </label>
+                <p/>
+                <input type="submit" value="Me connecter" class="button"/> <h6><a href="forgetPasswordController" class="forget">Mot de passe oublié ?</a> </h6>
+            </form>
+
+            <h2> PAS ENCORE DE COMPTE ? </h2>
+            - Créez un compte pour profiter de tous les avantages. <br/>
+            - Rejoignez le <a href ="#">programme de fidélité</a> dès votre premier achat. <p/>
+            
+            <input type="submit" value="Créer un compte" class="button"/>
+        </div>
     </div>
   );
 }
---
+++ src/DON'TREADME.md
@@ -0,0 +1,8 @@
+                <ul id="nav">
+                <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+                <a href="#"><i class="fa fa-fw fa-search"></i> Recherche</a> 
+                <a href="#"><i class="fa fa-fw fa-envelope"></i> Connexion</a> 
+                <a href="#"><i class="fa fa-fw fa-user"></i> Panier</a>
+                </ul>
+____________________
+
